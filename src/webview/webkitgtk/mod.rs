f672158e9ef040db847ebfdfc68e03e2e0c780f0
Iain Laird <iain@laird.org>
2022-03-27 18:29:34 +0100

    Fix webkitgtk implementation for download handlers

--- src/webview/webkitgtk/mod.rs
+++ src/webview/webkitgtk/mod.rs
@@ -224,8 +224,8 @@ impl InnerWebView {
       });
     }
 
-    if let Some(download_handler) = attributes.download_handler {
-      web_context.register_download_handler(download_handler)
+    if let Some(download_handlers) = attributes.download_handlers {
+      web_context.register_download_handler(download_handlers)
     }
 
     // Gtk application window can only contain one widget at a time.
